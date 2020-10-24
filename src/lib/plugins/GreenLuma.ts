dada7e4e920b55c05a3fa83df06b8c5d929225b8
Marc Solé <msolefonte@gmail.com>
2020-10-24 16:02:29 +0200

    Add GreenLuma support

---
+++ src/lib/plugins/GreenLuma.ts
@@ -0,0 +1,50 @@
+'use strict';
+
+import * as path from 'path';
+import {
+    CreamApiAchievementData,
+    CreamApiAchievementList,
+    Source,
+    UnlockedOrInProgressAchievement
+} from '../../types';
+import {SteamEmulatorScraper} from './utils/SteamEmulatorScraper';
+import {generateActiveAchievement} from './utils/Common';
+
+class CreamAPI extends SteamEmulatorScraper {
+    readonly source: Source = 'CreamAPI';
+    readonly achievementWatcherRootPath: string;
+    readonly achievementLocationFiles: string[] = [
+        'CreamAPI.Achievements.cfg'
+    ];
+
+    private readonly appDataPath: string = <string>process.env['APPDATA'];
+
+    constructor(achievementWatcherRootPath: string) {
+        super();
+        this.achievementWatcherRootPath = achievementWatcherRootPath;
+    }
+
+    normalizeActiveAchievements(achievementList: CreamApiAchievementList): UnlockedOrInProgressAchievement[] {
+        const activeAchievements: UnlockedOrInProgressAchievement[] = [];
+
+        Object.keys(achievementList).forEach((achievementName: string) => {
+            const achievementData: CreamApiAchievementData = achievementList[achievementName];
+            activeAchievements.push(
+                generateActiveAchievement(
+                    achievementName,
+                    achievementData.unlocktime * 1000000
+                )
+            );
+        });
+
+        return activeAchievements;
+    }
+
+    getSpecificFoldersToScan(): string[] {
+        return [
+            path.join(this.appDataPath, 'CreamAPI')
+        ];
+    }
+}
+
+export {CreamAPI};
