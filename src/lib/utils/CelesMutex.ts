9fc4358ea72fbe1cea46a60c7ccc8bdf2d9db4a3
Marc Solé <msolefonte@gmail.com>
2020-11-14 19:32:26 +0100

    Add v1.1.0 update
    - Remove fs-ext and reflect the new concurrency problem
    - Fix a bug that happend when a database game with invalid values was loaded
    - Add methods to the API of Celes
        - addGame()
        - removeManaullyAddedGame()
        - unlockAchievement()
        - removeManuallyUnlockedAchievement()
    - Add tests for the new methods
    - Minor fixes

--- src/lib/utils/CelesMutex.ts
+++ src/lib/utils/CelesMutex.ts
@@ -1,7 +1,15 @@
 import * as path from 'path';
 import {closeSync, promises as fs, openSync} from 'fs';
-import {flockSync} from 'fs-ext';
+// import {flockSync} from 'fs-ext';
 
+/**
+ * FIXME CelesMutex does not work anymore as we have not found any solution to implement an inter-process context-aware
+ *  mutex for Node.js. The previous iteration involved 'fs-ext' but it is not contex-aware, so a proper fix has to be
+ *  done.
+ *
+ *  More information about this problem can be found at this post of StackOverflow:
+ *  https://stackoverflow.com/questions/64801893/inter-process-context-aware-mutex-for-node-js
+ */
 export class CelesMutex {
     private readonly lockPath: string;
 
@@ -12,7 +20,8 @@ export class CelesMutex {
     async lock(): Promise<number> {
         await fs.mkdir(path.dirname(this.lockPath), { recursive: true });
         const fileDescriptor: number = openSync(this.lockPath, 'w');
-        flockSync(fileDescriptor, 'ex');
+        // FIXME
+        // flockSync(fileDescriptor, 'ex');
 
         return fileDescriptor;
     }
